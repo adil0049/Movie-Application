a6206960f352c1328605c68589658edaef557df0
Adil Hussain <adil0049@stud.kea.dk>
2017-05-04 11:27:27 +0200

    V2

--- Login.java
+++ Login.java
@@ -7,34 +7,63 @@ public class Login {
 
     }
 
+    public void  loginOrRegister() {
+        System.out.print("Do you want to login or register? Press 1 for login and 2 for register");
+        String input = System.console().readLine();
+
+        if (input.equals("1"))
+        Login();
+        else if (input.equals("2"))
+        createUser();
+        else {
+        System.out.println("Please try again");
+        loginOrRegister();
+        }
+
+
+        
+
+
+
+    }
+
     public void createUser() {
     
-    System.out.print("Enter name:");
-    String inputName = System.console().readLine();
+        System.out.print("Enter name:");
+        String inputName = System.console().readLine();
 
-    System.out.print("Enter email:");
-    String inputEmail = System.console().readLine();
+        System.out.print("Enter email:");
+        String inputEmail = System.console().readLine();
 
-    System.out.print("Enter pincode:");
-    int inputPincode = Integer.parseInt(System.console().readLine());
+        System.out.print("Enter pincode:");
+        int inputPincode = Integer.parseInt(System.console().readLine());
 
-     User user = new User(inputName, inputEmail, inputPincode);
+        User user = new User(inputName, inputEmail, inputPincode);
 
-    // Ovenstående inpit i new user er fra createAccount
+        // Ovenstående input i new user er fra createUser
 
-    users.add(user);
+        users.add(user);
     }
 
     public void Login() {
         
-    System.out.print("Enter email:");
-    String inputEmail = System.console().readLine();
+        System.out.print("Enter email:");
+        String inputEmail = System.console().readLine();
 
-    System.out.print("Enter pincode:");
-    int inputPincode = Integer.parseInt(System.console().readLine());
+        System.out.print("Enter pincode:");
+        int inputPincode = Integer.parseInt(System.console().readLine());
+
+        for (int i = 0; i < users.size(); i++) {
+      
+            if (inputEmail.equals(users.get(i).getEmail()) && inputPincode == users.get(i).getPincode() )
+            System.out.println("You are now logged in");
+            else 
+            System.out.println("Try again");
+
+        }
+    }
 
     
 
 
     }
-}
